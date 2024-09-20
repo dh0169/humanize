6506e15bdb075c3b47f5949cd2611d52c119e83c
bebop831 <bebop831@thunk>
2024-09-20 09:45:52 -0700

    Fixed CORS issues, should work with react now

--- src/__init__.py
+++ src/__init__.py
@@ -11,17 +11,16 @@ import os
 app = Flask(__name__)
 socketio = SocketIO()
 session_manager = SessionManager()
-CORS(app, origins=["http://localhost:5000", "http://127.0.0.1:5000"], supports_credentials=True, resources={r"/*": {"origins": "*"}})
 def create_app(debug=False):
 	"""Create an application."""
 	load_dotenv()
 	app.config.from_mapping(
 		SESSION_TYPE = 'filesystem',
 		SECRET_KEY = os.getenv("SECRET_KEY"),
-		SESSION_COOKIE_SECURE=True,
-    	SESSION_COOKIE_HTTPONLY=True,
-    	SESSION_COOKIE_SAMESITE='none',
-    	PERMANENT_SESSION_LIFETIME=timedelta(minutes=60),
+		# SESSION_COOKIE_SECURE=True,
+    	# SESSION_COOKIE_HTTPONLY=True,
+    	# SESSION_COOKIE_SAMESITE='none',
+    	# PERMANENT_SESSION_LIFETIME=timedelta(minutes=60),
 		DEBUG = debug
 	)
 	
@@ -34,7 +33,7 @@ def create_app(debug=False):
 	from .api import bp as api_blueprint
 	app.register_blueprint(api_blueprint)
 
-
+	CORS(app)
 	socketio.init_app(app, manage_session=False)
 
 
