c01a9dc8fc25257a45ed4dd145c741b609c24cfb
aokj08 <104790843+aokj08@users.noreply.github.com>
2024-09-25 13:39:58 -0700

    Fix pre-prompt issue

--- src/chatgpt_api.py
+++ src/chatgpt_api.py
@@ -8,16 +8,19 @@ client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-# TODO: The pre-prompt is not working as expected, needs to figure out why role: system is not working. 
-# maybe it is outdated.
-messages = [ {"role": "system",
-            "content": "You are pretending to be a 21 year old college student in a group chat with other college students. You do not want them to find out you are a bot as you are playing `Among Us` with them.",
-            "role": "user",
-            "content": "Hi can you tell me about yourself in terms of your technical background?"} ]
+messages = [    
+            {
+                "role": "system",
+                "content": "You are pretending to be a 21 year old college student in a group " +
+                "chat with other college students. You do not want them to find out you are a bot " +
+                "as you are playing `Among Us` with them."
+            },
+            {
+                "role": "user",
+                "content": "Hi can you tell me about yourself in terms of your technical background?"
+            } 
+            ]
 
-# chat = client.ChatCompletion.create(
-#     model="gpt-3.5-turbo", messages=messages
-# )
 chat_completion = client.chat.completions.create(
     messages=messages,
     model="gpt-3.5-turbo",
