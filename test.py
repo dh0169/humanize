cbb5bbf05bd1767984de6d6c4fcaa02caab01e74
aokj08 <104790843+aokj08@users.noreply.github.com>
2025-04-15 21:38:50 -0700

    added handle_msg test case

--- test.py
+++ test.py
@@ -28,6 +28,7 @@ def websockets_client(app, api_test_client):
     assert ws_client.is_connected('/chat')
     return ws_client
 
+# Test case 1 & 2
 def test_handle_connect_disconnect(websockets_client, api_test_client):    
     try:
         # Connect to chat
@@ -81,7 +82,7 @@ def test_handle_connect_disconnect(websockets_client, api_test_client):
         print("\nCleaning up...")
         api_test_client.get('/api/logout')
 
-
+# Test case 3
 def test_handle_join(app, websockets_client, api_test_client):
     try:
         # Register the user
@@ -117,6 +118,62 @@ def test_handle_join(app, websockets_client, api_test_client):
         print("\nResponse received from join after registering user: ")
         print(received)
         assert received['message'] == f"<User(id=1, username='{USER}', session_id=1, state=UserState.ACTIVE)> has connected to /chat"
+
+        websockets_client_after_registering_user.disconnect('/chat')
+
+
+    finally:
+        print("\nCleaning up...")
+        api_test_client.get('/api/logout')
+
+
+# Test case 4
+def test_handle_msg(app, websockets_client, api_test_client):
+    try:
+        # Register the user
+        response = api_test_client.post('/api/register', json={'username': USER})
+        print(response.json)
+        assert response.json['status'] == 'ok', "User register failed"
+
+        # Host a room
+        response = api_test_client.post('/api/lobby', json={'type': 'host', 'room': ROOM})
+        print("\nHosting room...\n")
+        print(response.json)
+        assert response.json['content'] is not None, "Room host failed"
+
+        # Connect to chat before registering user
+        websockets_client.connect('/chat')
+        assert websockets_client.is_connected('/chat')
+        websockets_client.emit('join', {'room': ROOM, 'username': USER}, namespace='/chat')
+        received = websockets_client.get_received('/chat')[0]['args']
+        print("\nResponse received from join before registering user: ")
+        print(received)
+        assert received['message'] == 'User is not registered', 'Lingering session, why is there a session before register?'
+        # Connect to chat now that we have a room and user
+        websockets_client_after_registering_user = socketio.test_client(app, flask_test_client=api_test_client)
+        websockets_client_after_registering_user.connect('/chat')
+        assert websockets_client_after_registering_user.is_connected('/chat')
+
+        # Join the room
+        print("\nJoining room...\n")
+        websockets_client_after_registering_user.emit('join', {'room': ROOM, 'username': USER}, namespace='/chat')
+        received = websockets_client_after_registering_user.get_received('/chat')[0]['args']
+        print("\nResponse received from join after registering user: ")
+        print(received)
+        assert received['message'] == f"<User(id=1, username='{USER}', session_id=1, state=UserState.ACTIVE)> has connected to /chat"
+
+        msg_data = {
+            'from': USER,
+            'room': ROOM,
+            'message': 'Hello world!'
+        }
+        websockets_client_after_registering_user.emit('message', msg_data, namespace='/chat')
+        received = websockets_client_after_registering_user.get_received('/chat')
+        print("\nResponse received after sending message: ")
+        print(received)
+        assert received[0]['args']['message'] == 'Hello world!', "Message not sent correctly"
+        assert received[0]['namespace'] == '/chat', "Namespace not correct"
+
 
         websockets_client_after_registering_user.disconnect('/chat')
 
